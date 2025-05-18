ef2fc2a35f7bdfb0542dfd25aec0162e9a97ec15
porawat chinpala <porawat.chi@metthier.co.th>
2025-05-18 20:52:44 +0700

    delete console

--- routes/category.js
+++ routes/category.js
@@ -4,12 +4,6 @@ import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
 
-// Log เพื่อ debug
-router.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} UserID: ${req.user?.id}`);
-    next();
-});
-
 router.get('/', verifyToken, getcategoryList);
 
 
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,8 @@
-// backend/routes/shop.js
 import express from 'express';
 import { getmyproduct, createProduct, updateProduct, deleteProduct } from '../controllers/productController.js';
 import { verifyToken } from '../middleware/auth.js';
 
 const router = express.Router();
-
-// Log เพื่อ debug
-router.use((req, res, next) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url} `);
-    next();
-});
 
 router.get('/:shopId', verifyToken, getmyproduct);
 router.post('/', verifyToken, createProduct);
